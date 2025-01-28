42854f8bfee5fb7cfcbfa960eb51d682e26f4be4
Matias <matias_gomez05@hotmail.com>
2025-01-28 13:57:36 -0300

    Skeleton y plantilla general Home

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,12 +1,12 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { Lilita } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${Lilita.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <GlobeAltIcon className="h-8 w-8 rotate-[15deg]" />
       <p className="text-[44px]">SET</p>
     </div>
   );
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, Lilita_One } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
+export const Lilita = Lilita_One({ subsets: ['latin'], weight: '400' });
