49aa0dfc6fd2f9f2fb427801b6508d14049ea873
Matias <matias_gomez05@hotmail.com>
2025-01-29 12:30:37 -0300

    Implementacion 'invoices' y elastic search con paginado

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,8 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
- 
+
+// Funcionalidad experimental de NextJS para mezclar sitios estaticos y contenido dinamico.
+// export const experimental_ppr = true;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
