5f4752c5a4ced380b936ac6e35d65cab6c868790
Matias Gomez <matias_gomez05@hotmail.com>
2025-02-04 16:38:10 -0300

    Create invoice -> manejo de errores client-side en el form si no seleccionan customers; ESLint para accesibilidad

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,34 +8,62 @@ import { sql } from '@vercel/postgres';
 
 /* Zod es una libreria para transformar los datos obtenidos desde el form, para eviar errores de validacion. 
  * Ej: type="number" dentro de un form siempre devuelve un string. Pero para validarlo correctamente en la bd se lo transforma a numero.
- * "coerce" fuerza un cambiar (o change)   
+ * "coerce" fuerza un cambiar (o change)  
+ * Tambien se puede configurar para que cada campo responda ante campos vacios o errores de tipeo. 
+ * Esto reemplaza la variable "required" de los formularios html. 
  */
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+      invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+      .number()
+      .gt(0, { message: 'Please enter an amount greater than $0.' }), //Caso vacio interpreta 0 al pasarlo a number()
+    status: z.enum(['pending', 'paid'], {
+      invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
-});
+  });
    
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
+export type State = {
+    errors?: {
+      customerId?: string[];
+      amount?: string[];
+      status?: string[];
+    };
+    message?: string | null;
+  };
+
  /* Para manipular muchos datos, se puede utilizar el metodo entries() con el objeto de JavaScript -> Object.fromEntries(). */
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
     try {
-        const { customerId, amount, status } = CreateInvoice.parse({
+        const validatedFields = CreateInvoice.safeParse({
             customerId: formData.get('customerId'),
             amount: formData.get('amount'),
             status: formData.get('status'),
         });
+
+        if (!validatedFields.success) {
+            return {
+                errors: validatedFields.error.flatten().fieldErrors,
+                message: 'Missing Fields. Failed to Create Invoice.',
+            };
+        }
+
+        const { customerId, amount, status } = validatedFields.data;
         const amountInCents = amount * 100;
-        const date = new Date().toLocaleString().split(',')[0];
+        const date = new Date().toISOString().split('T')[0];
 
         await sql `INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
     } catch (error) {
-        console.error(error);
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        };
     }
 
     revalidatePath('/dashboard/invoices');
